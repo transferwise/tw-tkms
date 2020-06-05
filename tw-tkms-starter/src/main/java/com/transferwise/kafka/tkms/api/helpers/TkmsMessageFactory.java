cc97a2d13b871ca766770d8a12f7ebc089bf2738
Kristo Kuusküll <onukristo@gmail.com>
2020-06-05 16:07:09 +0300

    ObjectMapper bean is not mandatory.

--- tw-tkms-starter/src/main/java/com/transferwise/kafka/tkms/api/helpers/TkmsMessageFactory.java
+++ tw-tkms-starter/src/main/java/com/transferwise/kafka/tkms/api/helpers/TkmsMessageFactory.java
@@ -4,13 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.transferwise.common.baseutils.ExceptionUtils;
 import com.transferwise.kafka.tkms.api.TkmsMessage;
 import java.nio.charset.StandardCharsets;
+import javax.annotation.PostConstruct;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TkmsMessageFactory implements ITkmsMessageFactory {
 
-  @Autowired
+  @Autowired(required = false)
   private ObjectMapper objectMapper;
+
+  @PostConstruct
+  public void init() {
+    // Older Spring ecosystems may not have it created as global bean.
+    if (objectMapper == null) {
+      objectMapper = new ObjectMapper();
+    }
+  }
 
   @Override
   public TkmsMessage createJsonMessage(@NonNull Object value) {
