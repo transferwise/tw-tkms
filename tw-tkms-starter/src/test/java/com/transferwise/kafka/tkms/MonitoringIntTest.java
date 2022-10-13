f12e46570146918393a42790b1bf01f02caa68b0
Péter Sinóros-Szabó <peter.sinoros-szabo@transferwise.com>
2022-10-13 16:50:44 +0200

    testing the Producer metrics

--- tw-tkms-starter/src/test/java/com/transferwise/kafka/tkms/MonitoringIntTest.java
+++ tw-tkms-starter/src/test/java/com/transferwise/kafka/tkms/MonitoringIntTest.java
@@ -1,13 +1,36 @@
 package com.transferwise.kafka.tkms;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.transferwise.common.baseutils.ExceptionUtils;
+import com.transferwise.kafka.tkms.api.ITransactionalKafkaMessageSender;
+import com.transferwise.kafka.tkms.api.TkmsMessage;
 import com.transferwise.kafka.tkms.test.BaseIntTest;
+import com.transferwise.kafka.tkms.test.TestMessagesListener;
+import com.transferwise.kafka.tkms.test.TestProperties;
 import io.micrometer.core.instrument.Gauge;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+@Slf4j
 class MonitoringIntTest extends BaseIntTest {
+
+  @Autowired
+  private ObjectMapper objectMapper;
+  @Autowired
+  private ITransactionalKafkaMessageSender transactionalKafkaMessageSender;
+  @Autowired
+  private TestMessagesListener testMessagesListener;
+  @Autowired
+  private TestProperties testProperties;
 
   @Test
   void testThatMonitoringMetricsArePresent() {
@@ -45,4 +68,37 @@ class MonitoringIntTest extends BaseIntTest {
     assertThat(meterRegistry.find("tw.tkms.dao.earliest.message.id").tags("shard", "0", "partition", "0").gauge())
         .as("Earliest message id tracking is not enabled for shard 0.").isNull();
   }
+
+  @Test
+  void testThatProducerMetricShowsSentMessage() throws Exception {
+
+    String message = "Hello World!";
+
+    AtomicInteger receivedCount = new AtomicInteger();
+    Consumer<ConsumerRecord<String, String>> messageCounter = cr -> ExceptionUtils.doUnchecked(() -> {
+      TestMessagesListener.TestEvent receivedEvent = objectMapper.readValue(cr.value(), TestMessagesListener.TestEvent.class);
+      if (receivedEvent.getMessage().equals(message)) {
+        receivedCount.incrementAndGet();
+      } else {
+        throw new IllegalStateException("Wrong message receive: " + receivedEvent.getMessage());
+      }
+    });
+
+    testMessagesListener.registerConsumer(messageCounter);
+    try {
+      TestMessagesListener.TestEvent testEvent = new TestMessagesListener.TestEvent().setId(1L).setMessage(message);
+
+      transactionalKafkaMessageSender
+              .sendMessage(new TkmsMessage().setTopic(testProperties.getTestTopic()).setValue(objectMapper.writeValueAsBytes(testEvent)));
+
+      await().until(() -> receivedCount.get() > 0);
+
+    } finally {
+      testMessagesListener.unregisterConsumer(messageCounter);
+    }
+
+    assertThat(meterRegistry.find("kafka.producer.record.send.total").tags("client.id", "producer-1", "kafka.version", "2.7.2").functionCounter().count())
+            .as("Producer's metric shows one message sent.").isEqualTo(1L);
+  }
+
 }
