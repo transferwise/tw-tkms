c9e66a89c3a8e60c842b09643e1af6ece614fe1a
Kristo Kuusküll <kristo.kuuskull@transferwise.com>
2023-05-11 13:19:48 +0300

    Fix.

--- tw-tkms-starter/src/main/java/com/transferwise/kafka/tkms/Debug.java
+++ tw-tkms-starter/src/main/java/com/transferwise/kafka/tkms/Debug.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.UtilityClass;
 
+/*
+   Mainly used to add verbose log to investigate specfic flaky tests.
+ */
 @UtilityClass
 public class Debug {
   
   /*
-    To allow to understand a flaky test failure.
+    To allow to understand a flaky test failure in `EarliestMessageTrackingIntTest.testIfEarliestMessageTrackerBehavesAsExpected`
    */
   @Getter
   @Setter
